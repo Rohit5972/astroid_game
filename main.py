cd2fdaaf716991ca7d831d038741a20db3dbb769
Rohit5972 <rohitkumar597277@gmail.com>
2025-01-05 11:33:40 +0530

    fps limiter

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock=pygame.time.Clock()
+    dt=0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        delta_time=clock.tick(60)/1000
+        dt+=delta_time
+        print(f"Delta Time: {delta_time:.4f}")
+
         screen.fill((0,0,0))
         pygame.display.flip()
 if __name__ == "__main__":
